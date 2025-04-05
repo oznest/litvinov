8a61fe52ea058e0fb8149a26ae9d5a39f70ffdc1
dev.aleksey.ch <dev.aleksey.ch@alldigitalads.com>
2025-04-05 13:08:02 +0300

    add mailhog

--- app/src/Controller/WeatherController.php
+++ app/src/Controller/WeatherController.php
@@ -7,11 +7,14 @@ use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use \Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 final class WeatherController extends AbstractController
 {
     public function __construct(
-        private readonly Weather $weather
+        private readonly Weather $weather,
+        private MailerInterface $mailer
     ){}
 
     /**
@@ -20,6 +23,13 @@ final class WeatherController extends AbstractController
     #[Route('/weather/{city}', name: 'city_weather')]
     public function index(?string $city = null): Response
     {
+        $email = (new Email())
+            ->from('test@example.com')
+            ->to('recipient@example.com')
+            ->subject('Test Email')
+            ->text('This is a test email.');
+
+        $this->mailer->send($email);
         try{
             $weather = $this->weather->getWeatherByCity($city);
 
