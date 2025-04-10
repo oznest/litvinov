de1c3595da79772eaaca13f789353caf1d600f4e
dev.aleksey.ch <dev.aleksey.ch@alldigitalads.com>
2025-04-10 12:58:31 +0300

    add fos user

---
+++ app/src/Admin/ProductAdmin.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class ProductAdmin extends AbstractAdmin
+{
+
+    protected function configureFormFields(FormMapper $formMapper): void
+    {
+        $formMapper
+            ->add('name')
+            ->add('category')
+            ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper): void
+    {
+        $listMapper
+            ->addIdentifier('name');
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper): void
+    {
+        $showMapper
+            ->add('name');
+    }
+}
